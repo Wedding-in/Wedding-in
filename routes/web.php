ce4b1f68afdcc022a280e4e53c941cbe592e28d9
Ipenkk <frankywilliam18@gmail.com>
2024-07-19 16:31:24 +0700

    inspiration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inspiration', function () {
+    return view('inspiration');
+});
