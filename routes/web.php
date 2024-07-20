62cffd5904459b3de5584ed86e572ccf27d2be29
AlvinaKrisendi <alvina.manggar@gmail.com>
2024-07-20 08:38:05 +0700

    First commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Detail_Vendor1', function () {
+    return view('detail_vendor_cake_venue_food');
+});
