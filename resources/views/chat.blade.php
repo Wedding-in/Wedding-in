2ecc03ec419a6d20a75a59d59910b6936ac7bd26
RodrickFelixson <rodrickfelixson@gmail.com>
2024-07-21 22:40:29 +0700

    update chat
    yg kemaren pake device 13 pro (salah)

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap');
@@ -24,24 +25,22 @@
     <div class="header container">
         <div class="row">
             <div class="col-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
-                </svg>
+                <i class="bi bi-arrow-left-short"></i>
             </div>
             <div class="col-4">
                 <h2>Vendor's Name</h2>
                 <h6>Location</h6>
             </div>
 
-            <div class="btn col-3" style="width: 40%">
+            <div class="btn col-3" style="width: 36%">
                 <button type="button" class="btn"><h6>Appointment</h6></button>
             </div>
         </div>
-        <hr>
+        <hr style="width: 115%">
     </div>
 
     {{-- BODY CHAT --}}
-    <div class="container">
+    <div class="chat container">
         <div class="vendor container">
             <div class="row">
                 <div class="col-1">
@@ -82,6 +81,12 @@
             </div>
         </div>
 
+        <div class="user container">
+            <div class="row">
+                <h6>Tau ah ullam temporibus enim, ipsa omnis. Illo fugit soluta id omnis adipisci numquam?</h6>
+            </div>
+        </div>
+
         <div class="type">
             <div class="row">
                 <div class="col">
@@ -90,7 +95,7 @@
                     </svg>
                 </div>
                 <div class="col">
-                    <textarea name="" id="" cols="30" rows="1">Type a message</textarea>
+                    <textarea name="" id="" cols="34" rows="1">Type a message</textarea>
                 </div>
                 <div class="col">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fcb2a9" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
