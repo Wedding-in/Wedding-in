ca2781e41c70cc013dffa02193ea61ef9494aa65
Alex <alexsandro.ardian45@gmail.com>
2024-08-18 21:07:32 +0700

    update name profile

--- resources/views/Profile/profile.blade.php
+++ resources/views/Profile/profile.blade.php
@@ -264,7 +264,7 @@
                     </div> --}}
                 </div>
                 <div class="w-50 text-right-namaprofile" style=" padding-left: 5px">
-                    <h5>nama</h5>
+                    <h5>Alvina</h5>
                     <a style="color: #FCB2A9" href="/detailprofile"><span>Edit Profile</span></a>
                 </div>
             </div>
