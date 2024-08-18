07fe135a2c471c9c481698f21792e99c6a958928
Ipenkk <frankywilliam18@gmail.com>
2024-08-18 17:26:32 +0700

    pull dr rodik

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,15 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
+        rel="stylesheet">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap');
@@ -21,12 +26,15 @@
 
     <title>CHAT</title>
 </head>
+
 <body>
     <div class="chat-container" style="background: #fffff8" id="blur">
         <div class="header">
             <div class="back-button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#ff5e5e" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#ff5e5e"
+                    class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
                 </svg>
             </div>
             <div class="vendor-info">
@@ -63,25 +71,29 @@
             </div>
         </div>
 
-        <button class="appointment-button" onclick="toggle()">Make appointment</button>
+        <button class="appointment-button" onclick="togglebtnApp()">Make appointment</button>
 
-        <div id="popup">
-            <h2>Thank you</h2>
-            <p>Your appointment has been successfully added.
-                You can check your list appointment on Profile menu.
-            </p>
-            <button class="appointment-button" onclick="toggle()">Close</button>
-        </div>
+            <div id="popup" class="popup" style="position: absolute; z-index: 9999;">
+                <h2>Thank you</h2>
+                <p>Your appointment has been successfully added.
+                    You can check your list appointment on Profile menu.
+                </p>
+                <a href="/chatlist">
+                    <button class="appointment-button" onclick="togglebtnApp()">Close</button>
+                </a>
+            </div>
+
+        <script type="text/javascript">
+            function togglebtnApp() {
+                var popup = document.getElementById('popup');
+                popup.classList.toggle('active');
+            }
+        </script>
+    </div>
     </div>
 </body>
 <script src="js/appointment.js" defer></script>
 
-<script type="text/javascript">
-    function toggle() {
-        var popup = document.getElementById('popup');
-        popup.classList.toggle('active');
-        var blur = document.getElementById('blur');
-        blur.classList.toggle('active');
-    }
-</script>
+
+
 </html>
