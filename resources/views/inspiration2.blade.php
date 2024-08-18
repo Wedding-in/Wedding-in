bcb134a420da2dcbb7096a820eb9979091d02680
Ipenkk <frankywilliam18@gmail.com>
2024-08-18 21:30:10 +0700

    coba

--- resources/views/inspiration2.blade.php
+++ resources/views/inspiration2.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Gallery</title>
-    <link rel="stylesheet" href="{{ asset('css/inspiration2.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/gallery2.css') }}">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -17,7 +17,7 @@
 <body>
     <div class="container">
         <div class="header">
-            <a href="/gallery">
+            <a href="/inspiration">
                 <i class="bi bi-arrow-left-short"></i>
             </a>
             <p>Inspiration</p>
