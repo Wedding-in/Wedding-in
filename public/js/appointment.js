8f984dfec7052a7707641af8f5ad6bd9d2983d9c
RodrickFelixson <rodrickfelixson@gmail.com>
2024-08-18 15:33:47 +0700

    update appointment

--- public/js/appointment.js
+++ public/js/appointment.js
@@ -1,14 +1,21 @@
 const currentDate = document.querySelector(".current-date"),
 daysTag = document.querySelector(".days"),
-prevNextIcon = document.querySelectorAll(".icons span");
+prevNextIcon = document.querySelectorAll(".icons span"),
+chosen1 = document.querySelector(".chosen1"),
+chosen2 = document.querySelector(".chosen2");
 
 
 let date = new Date(),
     currYear = date.getFullYear(),
-    currMonth = date.getMonth();
+    currMonth = date.getMonth(),
+    selectedDate = null,
+    selectedMonth = null,
+    selectedYear = null;
 
 const months = ["January", "February", "March", "April", "May", "June", "July",
                 "August", "September", "October", "November", "December"];
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const renderCalendar = () => {
     let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(),
@@ -22,9 +29,10 @@ const renderCalendar = () => {
     }
 
     for (let i = 1; i <= lastDateofMonth; i++) {
-        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-                            && currYear === new Date().getFullYear() ? "active" : "";
-        liTag += `<li class="${isToday}">${i}</li>`;
+        // let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+        //                     && currYear === new Date().getFullYear() ? "active" : "";
+        let isSelected = i === (selectedDate && currMonth === selectedMonth && currYear === selectedYear) ? "active" : "";
+        liTag += `<li class="${isSelected}">${i}</li>`;
     }
 
     for (let i = lastDayofMonth; i < 6; i++) {
@@ -33,6 +41,29 @@ const renderCalendar = () => {
 
     currentDate.innerHTML = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
+
+    addDateClickListeners();
+}
+
+const addDateClickListeners = () => {
+    const days = daysTag.querySelectorAll("li:not(.inactive)"); // Pilih hanya tanggal yang aktif
+
+    days.forEach(day => {
+        day.addEventListener("click", () => {
+            // Hapus kelas "active" dari elemen yang sebelumnya aktif
+            daysTag.querySelector(".active")?.classList.remove("active");
+            // Tambahkan kelas "active" ke elemen yang baru diklik
+            day.classList.add("active");
+            // Simpan tanggal yang dipilih
+            selectedDate = parseInt(day.innerText);
+            selectedMonth = currMonth;
+            selectedYear = currYear;
+
+            const selectedDayOfWeek = new Date(currYear, currMonth, selectedDate).getDay();
+            chosen1.innerHTML = `${months[selectedMonth]}`;
+            chosen2.innerHTML = `${daysOfWeek[selectedDayOfWeek]}, ${selectedDate}`;
+        });
+    });
 }
 
 renderCalendar()
