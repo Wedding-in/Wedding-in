9ef0b7caae0255d812118e86ee53735fc3a4bc8c
Ipenkk <frankywilliam18@gmail.com>
2024-08-18 21:28:40 +0700

    COBA

--- resources/views/inspiration2.blade.php
+++ resources/views/inspiration2.blade.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inspiration</title>
+    <title>Gallery</title>
     <link rel="stylesheet" href="{{ asset('css/inspiration2.css') }}">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     {{-- Bootstrap icons --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
 </head>
 <body>
     <div class="container">
         <div class="header">
-            <a href="/inspiration">
+            <a href="/gallery">
                 <i class="bi bi-arrow-left-short"></i>
             </a>
             <p>Inspiration</p>
         </div>
         <div class="isi">
-            <div class="card card_large" style="background-image: url('../img/5.jpeg'); background-size: cover;">
+            <div class="card card_large" style="background-image: url('../img/8.jpeg'); background-size: cover;">
                 <div class="bookmark">
                     <button id="btn">
                         <i class="bi bi-bookmark"></i>
